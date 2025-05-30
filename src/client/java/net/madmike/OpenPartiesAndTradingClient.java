6fbb3675eb9106789609a254a093c17879ce112c
Mr-iMikeyyy <mikesawesomevids@gmail.com>
2025-05-30 17:52:57 -0400

    lots of work to do

---
+++ src/client/java/net/madmike/OpenPartiesAndTradingClient.java
@@ -0,0 +1,10 @@
+package net.madmike;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OpenPartiesAndTradingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
